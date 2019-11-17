d45dab1852473ac97d52b8b5d311468ee680d71a
KIRIN3 <scomputer13@gmail.com>
2019-11-17 22:59:05 +0900

    Initial commit

---
+++ app/src/main/java/jp/kirin3/gittest/MainActivity.java
@@ -0,0 +1,15 @@
+package jp.kirin3.gittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
